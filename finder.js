a17734df9dbac297515aadaeaa7b524927de1264
Dzmitry_Karneyenka <Dzmitry_Karneyenka@epam.com>
2018-06-28 23:21:39 +0300

    add filter, write index.js code

--- finder.js
+++ finder.js
@@ -1,30 +1,75 @@
 'use strict';
-const request = require('request');
+const fs = require('fs');
+const Client = require('node-rest-client').Client;
+const client = new Client();
 
-const options = {
-    url: "https://rickandmortyapi.com/api/character/",
-    method: "GET",
-    json: true
-    
+let startUrl = "https://rickandmortyapi.com/api/character/";
+
+async function main (parameters) {
+    fs.writeFileSync('R&MDB.json', '{\"results\":[]}');
+    getBase(startUrl).then((result)=>{
+        //console.log(result);
+        setTimeout(()=>{
+            const character =  getCharacter(parameters);
+            console.log(character);},5000);
+    });
 }
-async function main(options, param) {
-    let dataBase = await getBase(options);
-    const json = JSON.stringify(dataBase);
-    console.log(param);
-    console.log(`DataBase: ${dataBase}`);
-    console.log(`DataBase: ${json}`);
 
-
-}
-async function getBase(options) {
-    return new Promise((resolve, reject) => {
+async function getBase(url) {
+    return new Promise((resolve, reject)=>{
         try {
-            //const  result = request(options,(err,  head, body)=>{console.log(body);});
-            //console.log(result.body);
-            return resolve(request(options).body);
+        client.get(url, (data, response) => {
+            if (response.statusCode === 200) {
+                let array = JSON.parse(fs.readFileSync('R&MDB.json','utf8')).results;
+                array = [...array,...data.results];
+                fs.writeFileSync('R&MDB.json','{\"results\": '+ JSON.stringify(array)+'}', 'utf-8');
+                url= data.info.next;
+                if (url!==''){getBase(url);}
+                //console.log('Data recieved!');
+            }
+            else {
+                console.log('Something went wrong! Response code:' + response.statusCode);
+            }
+        });
+        return resolve('success');
         } catch (error) {
             return reject(error);
         }
     });
+
+ 
+    //    client.get(url, (data, response) => {
+    //         if (response.statusCode === 200) {
+    //             let array = JSON.parse(fs.readFileSync('R&MDB.json','utf8')).results;
+    //             array = [...array,...data.results];
+    //             fs.writeFileSync('R&MDB.json','{\"results\": '+ JSON.stringify(array)+'}', 'utf-8');
+    //             url= data.info.next;
+    //             if (url!==''){getBase(data.info.next);}
+    //             //console.log('Data recieved!');
+    //         }
+    //         else {
+    //             console.log('Something went wrong! Response code:' + response.statusCode);
+    //         }
+    //     });
+
 }
-main (options);
+
+function getCharacter(parameters){
+    let dataBase = JSON.parse(fs.readFileSync('R&MDB.json','utf8')).results;
+    //console.log(dataBase);
+    // let result = dataBase.filter(element=>{
+    //     let ind = true;
+    //     Object.keys(parameters).forEach(elem => {
+    //         ind = element[elem]===parameters[elem]?true:false;
+    //         if (!ind) return false;
+    //     });
+    //     return ind;
+    // });
+    let result = '';
+    Object.keys(parameters).forEach(elem => {
+        result = dataBase.filter(element=>element[elem]===parameters[elem])
+    });
+    return result;
+}
+//main();
+exports.main = main;
---
+++ index.js
@@ -0,0 +1,32 @@
+'use strict';
+const finder = require('./finder.js');
+const args = require('yargs')
+    .usage('Usage: node $0 --id [string] --type [string]')
+    .example('$0 -n Alex').example('$0 --st Alive')
+    .option('id').alias('i', 'id').nargs('id', 1).describe('i', 'Number of ID')
+    .option('name').alias('n', 'name').nargs('n', 1).describe('n', 'Name of character')
+    .option('status').alias('st', 'status').nargs('st', 1).describe('st', 'Status: alive or dead')
+    .option('species').alias('sp', 'species').nargs('sp', 1).describe('sp', 'Species of character(e.g. Human)')
+    .option('type').alias('t', 'type').nargs('t', 1).describe('t', 'Type')
+    .option('gender').alias('g', 'gender').nargs('g', 1).describe('g', 'Gender: male or female')
+    .option('origin').alias('o', 'origin').nargs('o', 1).describe('o', 'Origin(e.g. Earth)')
+    .option('location').alias('l', 'location').nargs('l', 1).describe('l', 'Location(e.g. Earth)')
+    .help('h').alias('h', 'help')
+    .epilogue('Created by Dzmitry Karneyenka')
+    .argv
+
+let keys = ['id', 'name', 'species', 'status', 'type', 'gender', 'origin', 'location'];
+let result = '{'
+result += typeof(args.id)==='undefined'?'':'\"id\":'+args.id+',';
+result+= typeof(args.name)==='undefined'?'':'\"name\":\"'+args.name+'\",';
+result+= typeof(args.species)==='undefined'?'':'\"species\":\"'+args.species+'\",';
+result+= typeof(args.status)==='undefined'?'':'\"status\":\"'+args.status+'\",';
+result+= typeof(args.type)==='undefined'?'':'\"type\":\"'+args.type+'\",';
+result+= typeof(args.gender)==='undefined'?'':'\"gender\":\"'+args.gender+'\",';
+result+= typeof(args.origin)==='undefined'?'':'\"origin\":\"'+args.origin+'\",';
+result+= typeof(args.location)==='undefined'?'':'\"location\":\"'+args.location+'\",';
+//console.log(result);
+result = result.slice(0, -1)+'}';
+const parameters = JSON.parse(result);
+finder.main(parameters);
+//console.log(parameters);
